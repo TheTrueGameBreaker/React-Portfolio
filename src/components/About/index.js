af3240f41a22551c3e9e7d7a42238cb1cd5c3552
TheTrueGameBreaker <Davisgenthner@gmail.com>
2023-04-24 17:14:08 -0600

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./index.scss";
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+              idx={15}
+            />
+          </h1>
+          <p>
+            My Name is Davis Nigel Genthner I am currently 20 years old and have
+            about 4 years work experience in various restaurants and fast food
+            locations. I personally think that I learn fast, and bring a general
+            positive attitude wherever I go. I got along with co-workers pretty
+            well and Ive been considered decently funny by most of them. I
+            personally think that I learn fast, and bring a general positive
+            attitude wherever I go. I got along with co-workers pretty well and
+            Ive been considered decently funny by most of them.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default About;
