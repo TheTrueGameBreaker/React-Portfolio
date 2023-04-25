484f7c2de6e96d4c1197cf4f6d9be50609ad1018
TheTrueGameBreaker <Davisgenthner@gmail.com>
2023-04-24 19:06:54 -0600

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Layout from './components/Layout'
-import Portfolio from './components/Portfolio'
-import './App.scss'
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Layout from "./components/Layout";
+import Portfolio from "./components/Portfolio";
+import Contact from './components/Contact'
+import "./App.scss";
 
 function App() {
   return (
@@ -13,11 +13,12 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/portfolio" element={<Portfolio />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
