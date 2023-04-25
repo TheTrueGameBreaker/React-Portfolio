a0d90ab3f65efdad219e4610f38e12c1d7e43c8b
TheTrueGameBreaker <Davisgenthner@gmail.com>
2023-04-24 18:51:36 -0600

    third commit

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -8,6 +8,7 @@ import PWG from "../../assets/images/PasswordGen.png";
 import WDS from "../../assets/images/WDS.png";
 import Quiz from "../../assets/images/QuizGame.png";
 import PresentPear from "../../assets/images/PresentPear.png";
+import Hello from "../../assets/images/Hello.png";
 import "./index.scss";
 
 const Portfolio = () => {
@@ -26,26 +27,64 @@ const Portfolio = () => {
 
   const renderPortfolio = (portfolio) => {
     return (
-      <div class="image-box" style={{columnCount: 3}}>
+      <div class="image-box">
+        <Link to="https://hello--stranger.herokuapp.com/">
+          <p class="title">Hello stranger</p>
+          <p class="description">
+            {" "}
+            A simple application where users can share life stories.
+          </p>
+          <br></br>
+          <img src={Hello} class="portfolio-image"></img>
+        </Link>
         <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+          <p class="title">Weather App</p>
+          <p class="description">
+            A basic application that allows you to look up a city and get{" "}
+            <br></br> the 5 day weather forcast.
+          </p>
           <img src={Weather} class="portfolio-image"></img>
         </Link>
-        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+        <Link to="https://thetruegamebreaker.github.io/DND-Character-Generator/">
+          <p class="title">DnD Character Generator</p>
+          <p class="description">
+            A quick and easy dungeons and dragons character generator.
+          </p>{" "}
+          <br></br>
           <img src={DND} class="portfolio-image"></img>
         </Link>
-        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+        <Link to="https://pwa-davis-genthner.herokuapp.com/">
+          <p class="title">Text Editor</p>
+          <p class="description"> Just anothe text editor.</p>
           <img src={JATE} class="portfolio-image"></img>
         </Link>
-        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+        <Link to="https://thetruegamebreaker.github.io/DavisGenthnerPasswordGenerator/">
+          <p class="title">Password Generator</p>
+          <p class="description">
+            A password generator with many options for customization.
+          </p>
           <img src={PWG} class="portfolio-image"></img>
         </Link>
-        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+        <Link to="https://thetruegamebreaker.github.io/DavisGenthnerDailyPlanner/">
+          <p class="title">Work Day Planner</p>
+          <p class="description">A simple daily planner.</p>
           <img src={WDS} class="portfolio-image"></img>
         </Link>
-        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+        <Link to="https://thetruegamebreaker.github.io/Quiz-game/">
+          <p class="title">Coding Quiz Game</p>
+          <p class="description">
+            A very basic coding quiz game. I made this when I was just <br></br>{" "}
+            beggining so please forgive the easy questions and poor design
+          </p>
           <img src={Quiz} class="portfolio-image"></img>
         </Link>
-        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+        <Link to="https://present-pear.herokuapp.com/">
+          <p class="title">Present Pear</p>
+          <p class="description">
+            {" "}
+            A application that lets you anonymously <br></br> register to give
+            and recive gifts
+          </p>
           <img src={PresentPear} class="portfolio-image"></img>
         </Link>
       </div>
@@ -62,6 +101,10 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
+        <h2>
+          All Images are clickable and will take you to a deployed version of
+          the Project
+        </h2>
         <div>{renderPortfolio(portfolio)}</div>
       </div>
     </>
