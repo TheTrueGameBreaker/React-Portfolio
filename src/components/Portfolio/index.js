b2a613e40fb6be0d39ed348e26cf6768ef4034bc
TheTrueGameBreaker <Davisgenthner@gmail.com>
2023-04-24 18:09:59 -0600

    second commit

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
-import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
+import { Link } from "react-router-dom";
+import Weather from "../../assets/images/Weather.png";
+import DND from "../../assets/images/DND.png";
+import JATE from "../../assets/images/Jate.png";
+import PWG from "../../assets/images/PasswordGen.png";
+import WDS from "../../assets/images/WDS.png";
+import Quiz from "../../assets/images/QuizGame.png";
+import PresentPear from "../../assets/images/PresentPear.png";
 import "./index.scss";
 
 const Portfolio = () => {
@@ -19,8 +26,28 @@ const Portfolio = () => {
 
   const renderPortfolio = (portfolio) => {
     return (
-      <div className="images-container">
-        
+      <div class="image-box" style={{columnCount: 3}}>
+        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+          <img src={Weather} class="portfolio-image"></img>
+        </Link>
+        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+          <img src={DND} class="portfolio-image"></img>
+        </Link>
+        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+          <img src={JATE} class="portfolio-image"></img>
+        </Link>
+        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+          <img src={PWG} class="portfolio-image"></img>
+        </Link>
+        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+          <img src={WDS} class="portfolio-image"></img>
+        </Link>
+        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+          <img src={Quiz} class="portfolio-image"></img>
+        </Link>
+        <Link to="https://thetruegamebreaker.github.io/Weather-API-Project-Fixed/">
+          <img src={PresentPear} class="portfolio-image"></img>
+        </Link>
       </div>
     );
   };
@@ -37,7 +64,6 @@ const Portfolio = () => {
         </h1>
         <div>{renderPortfolio(portfolio)}</div>
       </div>
-      <Loader type="pacman" />
     </>
   );
 };
